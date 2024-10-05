302cf5c7b3f706750d61b26baad674878c244186
madhavpatel236 <madhavpatel236@gmail.com>
2024-10-05 12:06:44 +0530

    Make it more readable

--- Beyond the Bit (Backend)/utils/validator.js
+++ Beyond the Bit (Backend)/utils/validator.js
@@ -3,24 +3,28 @@ const validator = require("validator")
 
 const signupValidator = (req) => {
 
-    const { firstName, lastName, emailID, password, gender} = req.body
+    const { firstName, lastName, emailID, password, gender } = req.body
 
-    if(!firstName || firstName.length > 20){
+    if (!firstName || firstName.length > 20) {
         throw new Error("Please enter a valid user name")
-    } else if(!lastName || lastName.length > 30){
+    }
+    else if (!lastName || lastName.length > 30) {
         throw new Error("please enter a valid last name")
-    } else if(!emailID){
+    }
+    else if (!emailID) {
         throw new Error("please enter a emailID")
-    } else if(!validator.isStrongPassword(password)){
+    }
+    else if (!validator.isStrongPassword(password)) {
         throw new Error("please enter a strong password")
-    } else if( gender &&  !["male", "female", "others"].includes(gender)){
+    }
+    else if (gender && !["male", "female", "others"].includes(gender)) {
         throw new Error("please select from a male, female or others gender categories only")
     }
 
 
 
 
-}  
+}
 
 module.exports = {
     signupValidator
